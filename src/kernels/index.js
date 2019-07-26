565282a52ecd2effe0d0bfad060ce05da38a5060
Min Si Thu <archimedes557@gmail.com>
2019-07-26 21:43:48 +0630

    sobel operator feature

--- src/kernels/index.js
+++ src/kernels/index.js
@@ -4,6 +4,16 @@ let Kernels = {
         -1,  5, -1,
          0, -1,  0 
     ],
+    sobelHorizontalKernel:[
+                -1,0,1,	
+                -5,0,5,	
+                -1,0,1
+    ],
+    sobelVerticalKernel:[
+                -1,-5,-1,	
+                0,0,0,	
+                1,5,1
+    ]
 }
 
 module.exports = Kernels;
